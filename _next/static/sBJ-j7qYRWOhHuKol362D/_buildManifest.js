3ce7d02e4801dd8e09d3eb58957a90ad085fdc3b
zerorapid <zerorapid@users.noreply.github.com>
2026-05-15 16:05:36 +0000

    deploy: bc6d7ef663767a75ec9fe5fca4a1dec697cdabf7

---
+++ _next/static/sBJ-j7qYRWOhHuKol362D/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/zerosites-platform//_next/:path+",
+        "destination": "/zerosites-platform/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
